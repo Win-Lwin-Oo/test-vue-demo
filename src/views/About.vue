fb4751557eb598e8e56b8e115a564e94ac748e51
win <winlwinoocu@gmail.com>
2020-11-02 16:22:40 +0630

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div :class="{ active: isActive }">
+      Hello Vue JS
+    </div>
+    <div style="margin-top: 10px;" :class=" isActive ? 'active' : ''">
+      Hello World
+    </div>
+    <button style="margin-top:10px" @click="toogleBtn()">{{btnLabel}}</button>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+      btnLabel: "Active"
+    };
+  },
+  methods:{
+    toogleBtn(){
+      this.isActive = !this.isActive;
+      this.btnLabel = !this.isActive ? "Active" : "Deactive";
+    }
+  }
+};
+</script>
+<style scoped>
+.active {
+  background-color: yellow;
+}
+</style>
